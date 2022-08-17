5b12fdff899e2c87287a7d44522cca1b707a30b3
Argent <jrargent42@gmail.com>
2022-08-16 23:39:05 -0400

    finish arrays, set up random number generation, add validation checks

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,38 @@ var passwordNumber
 
 var passwordArray = []
 
+//arrays
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+//using Unicode for ASCII characters. Found a tutorial online for converting unicode to letters in an array
+//and then converting lowercase to uppercase
+const letterCodes = Array.from(Array(26)).map( (_, i) => i + 97);
+const lowercaseLetters = letterCodes.map(code => String.fromCharCode(code));
+const uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
+
+
+// Write password to the #password input
+function writePassword() {
+  reset();
+  //local variables - only accessible by this function
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+//function for generating the password itself
 function generatePassword() {
   console.log("Button was clicked");
 
   passLengthPrompt();
-  console.log(passwordLength)
+    console.log(passwordLength)
 
   // check user input meets criteria of number of characters between 8 and 128
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) || passwordLength == null) {
@@ -33,48 +60,54 @@ function generatePassword() {
     passLengthPrompt();
     console.log(passwordLength);
   }
-
   passUpperPrompt();
-  console.log(passwordUppercase);
-    if (passwordUppercase = true) {
-      //this section will generate random lowercase letters for password
-    }
-    else {
-      //may not actually need an else?
-    }
-
+    console.log(passwordUppercase);
+  
   passLowerPrompt();
-  console.log(passwordLowercase);
-    if (passwordLowercase = true) {
-      //this section will generate random uppercase letters for password
-    }
-    else {
-      //may not actually need an else?
-    }
-
+    console.log(passwordLowercase);
+    
   passSpecCharPrompt();
-  console.log(passwordSpecChar);
-    if (passwordSpecChar = true) {
-        //this section will generate random special characters for password
-    }
-    else {
-      //not sure yet if need an else here
-    }
-
+    console.log(passwordSpecChar);
+   
   passNumberPrompt();  
-  console.log(passwordNumber);
-    if (passwordNumber = true) {
-      //this section will generate random numbers for password
+    console.log(passwordNumber);
+   
+    // validation for selecting at least one true value from the prompts
+    if (passwordUppercase || passwordLowercase || passwordSpecChar || passwordNumber) {
+
+      //for loop goes through function for creating a random number between 1 and 4. 
+      //If number is 1 AND passwordUppercase is true, then random number function goes through 
+      //and picks a random selection from listed array
+      for(let i = 0; i < passwordLength; i++) {
+    
+      var charType = randomIntFromInterval(1,4);
+          if(charType == 1 && passwordUppercase){
+              passwordArray.push(uppercaseLetters[randomIntFromInterval(0, uppercaseLetters.length - 1)])
+          }
+            else if(charType == 2 && passwordLowercase){
+              passwordArray.push(lowercaseLetters[randomIntFromInterval(0, lowercaseLetters.length - 1)]) 
+          }
+            else if(charType == 3 && passwordNumber){
+              passwordArray.push(numbers[randomIntFromInterval(0, numbers.length - 1)])//fix
+          }
+            else if(charType == 4 && passwordSpecChar){
+              passwordArray.push(symbols[randomIntFromInterval(0, symbols.length - 1)])
+            }
+
+          else{
+              i--;
+              }
+        }
+
+        return(passwordArray.join('')) // returns password into generate password, which passes into password variable.
     }
+      // if at least one prompt is not selected, generate password function will start again.
     else {
-      //not actually sure if need an else?
-    }
+      window.alert("Please select at least one password criteria option.")
+      return generatePassword();
+      }
+  }
 
-    //need to include a way to verify that user has selected at least one true statement from prompt. 
-    //All prompts cannot be false or no password will be generated.
-
-  return "Generated password will go here!"; //return passwordArray after join?
-}
 
 //functions for prompting user
 function passLengthPrompt() {
@@ -98,16 +131,18 @@ function passNumberPrompt() {
   passwordNumber = confirm("Do you want your password to include numbers?")
 }
 
-
-// Write password to the #password input
-function writePassword() {
-  //local variables - only accessible by this function
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// function for randomly generating numbers for the arrays
+function randomIntFromInterval(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1) + min) // 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// resets variables back to default, which is false
+function reset() {
+
+passwordLength = false;
+passwordUppercase = false;
+passwordLowercase = false;
+passwordSpecChar = false;
+passwordNumber = false;
+passwordArray = [];
+}
